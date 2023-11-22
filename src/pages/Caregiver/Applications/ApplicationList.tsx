9bb060702892f280b184ef8d880e29439a904a6e
RamboXD <rnazymxan@gmail.com>
2023-11-22 22:14:24 +0600

    asd

--- src/pages/Caregiver/Applications/ApplicationList.tsx
+++ src/pages/Caregiver/Applications/ApplicationList.tsx
@@ -38,44 +38,45 @@ const ApplicationList: React.FC = () => {
     <Layout>
       <p className="text-xl mb-5 font-semibold">Application List</p>
       <div>
-        {applications.map((application) => (
-          <div
-            key={application.JobDetails.RequiredCaregivingType}
-            className="border rounded p-4 mb-4"
-          >
-            <p>
-              Job Required Caregiving Type:{" "}
-              {application.JobDetails.RequiredCaregivingType}
-            </p>
-            <p>
-              Date Applied:{" "}
-              {new Date(application.DateApplied).toLocaleDateString()}
-            </p>
-            <button
-              onClick={() =>
-                toggleApplicationDetails(application.JobDetails.JobID)
-              }
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
+        {applications &&
+          applications.map((application) => (
+            <div
+              key={application.JobDetails.RequiredCaregivingType}
+              className="border rounded p-4 mb-4"
             >
-              {selectedApplication === application.JobDetails.JobID
-                ? "Hide Details"
-                : "Show Member Details"}
-            </button>
-            {selectedApplication === application.JobDetails.JobID && (
-              <div>
-                <p>
-                  Member Email:{" "}
-                  {application.JobDetails.MemberDetails.UserDetails.Email}
-                </p>
-                <p>
-                  Member Name:{" "}
-                  {application.JobDetails.MemberDetails.UserDetails.GivenName}{" "}
-                  {application.JobDetails.MemberDetails.UserDetails.Surname}
-                </p>
-              </div>
-            )}
-          </div>
-        ))}
+              <p>
+                Job Required Caregiving Type:{" "}
+                {application.JobDetails.RequiredCaregivingType}
+              </p>
+              <p>
+                Date Applied:{" "}
+                {new Date(application.DateApplied).toLocaleDateString()}
+              </p>
+              <button
+                onClick={() =>
+                  toggleApplicationDetails(application.JobDetails.JobID)
+                }
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
+              >
+                {selectedApplication === application.JobDetails.JobID
+                  ? "Hide Details"
+                  : "Show Member Details"}
+              </button>
+              {selectedApplication === application.JobDetails.JobID && (
+                <div>
+                  <p>
+                    Member Email:{" "}
+                    {application.JobDetails.MemberDetails.UserDetails.Email}
+                  </p>
+                  <p>
+                    Member Name:{" "}
+                    {application.JobDetails.MemberDetails.UserDetails.GivenName}{" "}
+                    {application.JobDetails.MemberDetails.UserDetails.Surname}
+                  </p>
+                </div>
+              )}
+            </div>
+          ))}
       </div>
     </Layout>
   );
